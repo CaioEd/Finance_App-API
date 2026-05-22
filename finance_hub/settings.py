6af9a9adcc0f03786f4697a1bf8bc6aecca12ee7
CaioEd <caioersb@gmail.com>
2026-05-22 20:16:17 -0300

    fix: settings/.env/ add .env example

--- finance_hub/settings.py
+++ finance_hub/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'finance_hub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB'),
-        'USER': 'root',
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT')
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
